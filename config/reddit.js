b886f26b897bc1e5c9dcea24d19d70afc87fca71
Robert Dyjas <15113729+robertdyjas@users.noreply.github.com>
2018-01-16 23:02:09 +0100

    Create reddit.js

---
+++ config/reddit.js
@@ -0,0 +1,9 @@
+// This is an example file. Be sure to rename it to local.js and add valid credentials.
+module.exports = {
+  reddit: {
+    tradeSub: 'tradesubname',
+    tradeSubShort: 'tsub',
+    eggSub: 'eggsubnams',
+    eggSubShort: 'esub'
+  }
+}
